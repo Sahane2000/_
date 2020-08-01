3c5fa633726c29891b340a7b27f6243b199e80bc
Atharva Hase <56650939+Atharvahase@users.noreply.github.com>
2020-08-01 17:35:35 +0530

    Create dashboard1.php

---
+++ dashboard1.php
@@ -0,0 +1,20 @@
+<?php
+require('db.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Dashboard - Secured Page</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<div class="form">
+<h1>Welcome to Kissan Help.....</h1>
+<h3><a href="index2.html">Home</a><h3>
+<p><a href="insert.php">If New User Then Click Here....</a></p>
+<p><a href="view1.php">Existing User Click Here.....</a><p>
+<p><a href="logout.php">Logout</a></p>
+</div>
+</body>
+</html>

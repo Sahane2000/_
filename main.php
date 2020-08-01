870a62044ed4ef0797cea279b2804fcb8fa51f0b
Sahane2000 <sahanepragati05@gmail.com>
2020-08-01 19:09:28 +0530

    Created main.php

---
+++ main.php
@@ -0,0 +1,20 @@
+<?php
+require('db.php');
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8">
+<title>Welcome</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body >
+<div class="form">
+<h1 align="center">Welcome to Kissan Help.....</h1>
+<h1 align="center"><a href="login.php">Farmer Login</a><h1>
+<h1 align="center"><a href="alogin.php">You Are Admin Then Click Here....</a></h1>
+
+</div>
+</body>
+</html>

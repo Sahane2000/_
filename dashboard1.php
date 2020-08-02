31eb9f1eac4beada0bbfcdf0b4230b99910b5069
Atharva Hase <56650939+Atharvahase@users.noreply.github.com>
2020-08-02 16:35:52 +0530

    Update dashboard1.php

--- dashboard1.php
+++ dashboard1.php
@@ -4,18 +4,30 @@ require('db.php');
 <!DOCTYPE html>
 <html>
 <head>
+<style>
+body {
+  background-image: url('img.jpeg');
+  background-repeat: no-repeat;
+  background-attachment:fixed;
+  background-size:cover;
+  height:600px;
+}
+a
+{
+	color:#990012;
+}
+</style>
 <meta charset="utf-8">
 <title>Dashboard - Secured Page</title>
 <link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
 <div class="form">
-<h1>Welcome to Kissan Help.....</h1>
-<h3><a href="index2.html">Home</a><h3>
-<p><a href="insert.php">If New User Then Click Here....</a></p>
-<p><a href="view1.php">Existing User Click Here.....</a><p>
-<p><a href="logout.php">Logout</a></p>
+<h1 align="center">Welcome to Kissan Help.....</h1>
+<h2 align="center"><a href="index2.html">Home</a><h2>
+<h2 align="center"><a href="insert.php">If New User Then Click Here....</a></h2>
+<h2 align="center"><a href="view1.php">Existing User Click Here.....</a><h2>
+<h2 align="center"><a href="logout.php">Logout</a></h2>
 </div>
 </body>
 </html>
-

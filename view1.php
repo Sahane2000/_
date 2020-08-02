be825b7513f280698f85f5f688e8dc0cce7409a9
Atharva Hase <56650939+Atharvahase@users.noreply.github.com>
2020-08-02 16:24:26 +0530

    Update view1.php

--- view1.php
+++ view1.php
@@ -4,21 +4,52 @@ require('db.php');
 <!DOCTYPE html>
 <html>
 <head>
+<style>
+table,tr{
+  border:3px solid black;
+  
+  border-spacing:5px;
+  background-color:#dfcfbe;
+}
+td
+{
+  font-family:Arial;
+  font-size:15pt;
+}
+body {
+  background-image: url('img.jpeg');
+  background-repeat: no-repeat;
+  background-attachment:fixed;
+  background-size:cover;
+  height:600px;
+}
+</style>
 <meta charset="utf-8">
-<title>View Records</title>
+<h2><title align='center'>View Records</title></h2>
 <link rel="stylesheet" href="css/styles.css" />
 </head>
 <body>
 	<form method="post" onsubmit="submit">
-	<label>Enter your Registered Mobile No</label>
-	<input type=text name="mobile1">
+	<table align='center'>
+	<tr>
+	<td>
+	<label align='center'>Enter your Registered Mobile No</label>
+	</td>
+	<td>
+	<input type=text aloign='center'name="mobile1">
+	</td>
+	</tr>
+	<tr>
+	<td>
 	 <input type="submit"name="submit" class="button" value="Submit">
+	 </td>
+	 </tr>
 	</form>
 <div class="form">
-<p><a href="index2.php">Home</a> 
+<h2 align='center'><a href="index2.php">Home</a> 
 | <a href="insert.php">Insert New Record</a> 
-| <a href="logout.php">Logout</a></p>
-<h2>View Records</h2>
+| <a href="logout.php">Logout</a></h2>
+<h2 align='center'>View Your Records </h2>
 <table width="100%" border="1" style="border-collapse:collapse;">
 <thead>
 <tr>
@@ -48,7 +79,7 @@ if (isset($_REQUEST["submit"]))
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 $count=1;
-$sel_query="Select * from farmerinfo where mobile1=$mobile1";
+$sel_query="Select * from farmerinfo where mobile1 = '$mobile1'";
 $result = mysqli_query($conn,$sel_query);
 while($row = mysqli_fetch_assoc($result)) { ?>
 <tr><td align="center"><?php echo $count; ?></td>
@@ -77,5 +108,3 @@ while($row = mysqli_fetch_assoc($result)) { ?>
 </div>
 </body>
 </html>
-
-

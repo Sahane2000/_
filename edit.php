24ef8f311825725a83453a99ebd529a1af7dc836
Atharva Hase <56650939+Atharvahase@users.noreply.github.com>
2020-08-02 16:33:37 +0530

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,6 +1,6 @@
 <?php
 require('db.php');
-$idd=$_REQUEST['id'];
+
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -15,16 +15,36 @@ $row = mysqli_fetch_assoc($result);
 <!DOCTYPE html>
 <html>
 <head>
+<style>
+body {
+  background-image: url('agriculture.jpeg');
+  background-repeat: no-repeat;
+  background-attachment:fixed;
+  background-size:cover;
+  height:600px;
+}
+table,tr{
+  border:1px solid black;
+  
+  border-spacing:5px;
+  background-color:#dfcfbe;
+}
+td
+{
+  font-family:Arial;
+  font-size:15pt;
+}
+</style>
 <meta charset="utf-8">
 <title>Update Record</title>
 <link rel="stylesheet" href="css/style.css" />
 </head>
-<body>
+<body style="background-color:grey;">
 <div class="form">
-<p><a href="dashboard1.php">Dashboard</a> 
+<h2 align="center"><a href="dashboard1.php">Dashboard</a> 
 | <a href="insert.php">Insert New Record</a> 
-| <a href="logout.php">Logout</a></p>
-<h1>Update Record</h1>
+| <a href="logout.php">Logout</a></h2>
+<h1 align='center'>Update Record</h1>
 <?php
 $status = "";
 if(isset($_POST['new']) && $_POST['new']==1)
@@ -37,8 +57,9 @@ $region =$_REQUEST['region'];
 $sno =$_REQUEST['sno'];
 $aiker =$_REQUEST['aiker'];
 $type =$_REQUEST['type'];
-$crop1 =$_REQUEST['crop1'];
-$update="update farmerinfo set name='".$name."', address='".$address."', mobile1='".$mobile1."', sno='".$sno."'where mobile1='".$mobile1."'";
+$crop =$_REQUEST['crop'];
+$stage =$_REQUEST['stage'];
+$update="update farmerinfo set aiker='".$aiker."', type='".$type."', crop='".$crop."', stage='".$stage."'where mobile1='".$mobile1."'";
 mysqli_query($conn, $update) or die(mysqli_error($conn));
 $status = "Record Updated Successfully. </br></br>
 <a href='view1.php'>View Updated Record</a>";
@@ -47,21 +68,40 @@ echo '<p style="color:#FF0000;">'.$status.'</p>';
 ?>
 <div>
 <form name="form" method="post" action=""> 
+<table align='center'>
+<tr>
+<td>
 <input type="hidden" name="new" value="1" />
 <input name="id" type="hidden" value="<?php echo $row['id'];?>" />
-<p><input type="text" name="name" placeholder="Enter Name" 
-required value="<?php echo $row['name'];?>" /></p>
-<p><input type="text" name="address" placeholder="Enter Address" 
-required value="<?php echo $row['address'];?>" /></p>
-<p><input type="text" name="mobile1" placeholder="Enter Mobile" 
-required value="<?php echo $row['mobile1'];?>" /></p>
-<p><input type="text" name="sno" placeholder="Enter Satbara number" 
-required value="<?php echo $row['sno'];?>" /></p>
-<p><input name="submit" type="submit" value="Update" /></p>
+</td>
+</tr>
+<tr>
+<td>
+<h4>Enter Acre<input type="text" name="aiker" placeholder="Enter Acre Details" 
+required value="<?php echo $row['aiker'];?>" /></h4>
+</td>
+</tr>
+
+<tr>
+<td>
+<h4>Enter Crop<input type="text" name="crop" placeholder="Enter Crop" 
+required value="<?php echo $row['crop'];?>" /></h2>
+</td>
+</tr>
+<tr>
+<td>
+<h4>Enter Stage<input type="text" name="stage" placeholder="Enter Crop Stage" 
+required value="<?php echo $row['stage'];?>" /></h4>
+</td>
+</tr>
+<tr>
+<td>
+<h4 align="center"><input name="submit" type="submit" class="btn btn-success"value="Update" /></h4>
+</td>
+</tr></table>
 </form>
 <?php } ?>
 </div>
 </div>
 </body>
-</html>  
-
+</html>
